2a8774fe53f5d2cab0e56c6f878450613c38c177
Phan Minh Hai <haipmbdvn@gmail.com>
2019-11-28 17:21:33 +0700

    chinhsualan2

--- myproject/database/migrations/2019_10_14_162059_create_lich_su_mua_credits_table.php
+++ myproject/database/migrations/2019_10_14_162059_create_lich_su_mua_credits_table.php
@@ -16,10 +16,7 @@ class CreateLichSuMuaCreditsTable extends Migration
         Schema::create('lich_su_mua_credits', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('nguoi_choi_id');
-            $table->foreign('nguoi_choi_id')->references('id')->on('nguoi_chois');
             $table->integer('goi_credit_id');
-            $table->foreign('goi_credit_id')->references('credit')->on('nguoi_chois');
-            $table->foreign('goi_credit_id')->references('credit')->on('goi_credits');
             $table->timestamps();
         });
     }
